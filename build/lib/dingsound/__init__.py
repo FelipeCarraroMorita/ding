111358eaf51e580ab56fade23fe8d9824268e064
FelipeMorita <31017596+FelipeCarraroMorita@users.noreply.github.com>
2021-07-23 19:34:56 -0300

    Delete build/lib directory

--- build/lib/dingsound/__init__.py
+++
@@ -1,1 +0,0 @@
-from dingsound.functions import ding
--- build/lib/dingsound/functions.py
+++
@@ -1,7 +0,0 @@
-import vlc
-import time
-
-def ding():
-    p = vlc.MediaPlayer("http://tale3habet.eb2a.com/sounds/kitchen%20timer%20done.mp3")
-    p.play()
-    time.sleep(9)
